9642f949198498b721322bee989d383d61d163fc
Ruben-Alvarez-Dev <ruben.alvarez.dev@gmail.com>
2024-03-29 12:48:09 +0100

    description elements

--- src/components/main/Info.tsx
+++ src/components/main/Info.tsx
@@ -1,4 +1,5 @@
 import { FaRegPlayCircle } from 'react-icons/fa';
+import { FaStar } from 'react-icons/fa';
 import { ContainerInfo } from '../styled.components';
 
 export const Info = () => {
@@ -16,6 +17,21 @@ export const Info = () => {
               <div>2020</div>
               <div>Fantastic</div>
               <div>2h 35min</div>
+            </section>
+            <section className="star">
+              <FaStar size={25} color={"orange"}/>
+              <FaStar size={25} color={"orange"}/>
+              <FaStar size={25} color={"orange"}/>
+              <FaStar size={25} color={"orange"}/>
+              <FaStar size={25} color={"orange"}/>
+            </section>
+            <section className="description"> 
+              <div className="description-title">
+                Plot Sumary
+              </div>
+              <div className="desc">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem ab voluptate dolor vel architecto laboriosam dolorem aliquid similique distinctio nihil, ea impedit nulla totam quaerat, hic officiis molestiae dicta asperiores!
+              </div>  
             </section>
           </section>
         </ContainerInfo>
