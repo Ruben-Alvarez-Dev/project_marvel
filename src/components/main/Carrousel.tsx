083e12628728bd5a8d19f9594ed60c57a66ce24f
Ruben-Alvarez-Dev <ruben.alvarez.dev@gmail.com>
2024-03-29 02:06:42 +0100

    fetching and useEffect working fine

--- src/components/main/Carrousel.tsx
+++ src/components/main/Carrousel.tsx
@@ -1,5 +1,20 @@
+import { useEffect } from 'react'
 import { Container } from '../styled.components'
+
 export const Carrousel = () => {
+
+  const getData = async () => {
+    const url = import.meta.env.VITE_BASE_URL;
+    const items = await fetch (url);
+    const response = await items.json();
+    console.log(response);
+  }
+
+  useEffect(() => {
+    getData();
+    console.log('Carrousel')
+  }, [])
+
   return (
     <>
       <Container height={50}>
